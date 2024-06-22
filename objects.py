3935f5b365b51daec29b57e90b62efedeba1ba33
Israel-0 <israeldoradoavila@gmail.com>
2024-06-22 08:26:49 +0200

    Implementation of aliens
    Creation of Aliens and their methods for shotting and move

--- objects.py
+++ objects.py
@@ -1,11 +1,11 @@
-import pygame
-from screen import screen_height
+import pygame,random
+from screen import screen_height,screen_widht
 
 class Player(pygame.sprite.Sprite):
     def __init__(self,pos,constraint,speed):
         super().__init__()
         pygame.sprite.Sprite.__init__(self)
-        self.image=pygame.image.load('sprites/player.png').convert_alpha()
+        self.image=pygame.image.load('resources/player.png').convert_alpha()
         self.rect = self.image.get_rect(midbottom=(pos))
         self.speed = speed
         self.max_x_constraint = constraint
@@ -39,6 +39,10 @@ class Player(pygame.sprite.Sprite):
 
     def shoot_laser(self):
         self.lasers.add(Laser(self.rect.center,))
+
+    def reset(self):
+        self.rect = self.image.get_rect(midbottom=(screen_widht/2, screen_height))
+        self.lasers.empty()
 
     def update(self):
         self.get_input()
@@ -99,3 +103,52 @@ class Obstacle():
                     pos_y = y + row * 3
                     block = Block((pos_x,pos_y))
                     self.blocks_group.add(block)
+
+class Alien(pygame.sprite.Sprite):
+    def __init__(self,spri,x,y):
+        super().__init__()
+        pygame.sprite.Sprite.__init__(self)
+        self.type = spri
+        self.path = f'resources/{spri}.png'
+        self.image=pygame.image.load(self.path).convert_alpha()
+        self.rect = self.image.get_rect(topleft=(x,y))
+        self.exploding = False
+        self.explosion_time = 0
+        if spri == 'green':
+            self.explosion_image = pygame.image.load('resources/explosion_green.png').convert_alpha()
+        elif spri == 'yellow':
+            self.explosion_image = pygame.image.load('resources/explosion_yellow.png').convert_alpha()
+        else:
+            self.explosion_image = pygame.image.load('resources/explosion_red.png').convert_alpha()
+
+    def explode(self):
+        self.image = self.explosion_image
+        self.exploding = True
+        self.explosion_time = pygame.time.get_ticks()
+    def update(self, direction):
+        if not self.exploding:
+            self.rect.x += direction
+        else:
+            self.rect.x += direction
+            current_time = pygame.time.get_ticks()
+            if current_time - self.explosion_time > 500:
+                self.kill()
+
+class MisteryShip(pygame.sprite.Sprite):
+    def __init__(self,screen_width):
+        super().__init__()
+        pygame.sprite.Sprite.__init__(self)
+        self.scren_width = screen_width
+        self.image = pygame.image.load('resources/mystery.png').convert_alpha()
+        self.x = random.choice([0, self.scren_width - self.image.get_width()])
+        if self.x == 0:
+            self.speed = 3
+        else:
+            self.speed = -3
+        self.rect = self.image.get_rect(topleft=(self.x,40))
+    def update(self):
+        self.rect.x += self.speed
+        if self.rect.right > self.scren_width:
+            self.kill()
+        elif self.rect.left < 0:
+            self.kill()
